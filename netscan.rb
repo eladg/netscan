752442635b35d5ae1798ddf74b51a5511bc6af57
Or Gariany <orgariany@gmail.com>
2014-03-30 03:01:42 +0300

    parse args

--- netscan.rb
+++ netscan.rb
@@ -1,6 +1,7 @@
-#!/usr/bin/ruby
+#!/home/user/.rvm/rubies/ruby-2.1.0/bin/ruby
 
 require 'pry'
+require 'optparse'
 
 def show_help()
   puts ""
@@ -13,6 +14,7 @@ def show_help()
   puts "-type scan type [full,stealth,fin,ack]"
   puts "-b    bannerGrabber status (Should work only for TCP)"
   puts ""
+  exit
 end
 
 def usage()
@@ -31,6 +33,30 @@ def main(args)
   if args[0] == "-h"
     show_help()
   end
+
+  counter = 0
+  options = Hash.new
+
+  args.each do |argument|
+
+    if argument[0] == '-'
+      theArg = argument.dup
+      theArg[0] = ''
+      if args[counter+1].include? ','
+        theValue = args[counter+1].dup
+        options[theArg] = theValue.split ','
+      elsif args[counter+1].include? '-'
+        theValue = args[counter+1].dup
+        options[theArg] = theValue
+      else
+        options[theArg] = args[counter+1]
+      end 
+      
+    end
+
+    counter+=1
+  end
+
 
 end
 
