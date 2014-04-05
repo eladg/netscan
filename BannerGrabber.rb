6e7ff736eff1a1a6a1fb90290ac5bb904d9ee81e
orgariany <orgariany@gmail.com>
2014-04-05 20:01:08 +0300

    Bug fixes

--- BannerGrabber.rb
+++ BannerGrabber.rb
@@ -10,32 +10,46 @@ class BannerGrabber
   end
   
   def analyze_host
-    result = analyze_web
-    if result == "unknown OS"
-      result = analyze_ftp
+    @result = analyze_web
+    if @result == "unknown OS"
+      @result = analyze_ftp
     end
-    return result
+    return @result
   end
 
   #creates a TCP socket over port 80 and submit a get request.
   #it is looking for the string: "server:" and decides what is the operating system of that server.
   def analyze_web
-    socket = Socket.new( AF_INET, SOCK_STREAM, 0 )
+    @socket = Socket.new( AF_INET, SOCK_STREAM, 0 )
     sockaddr = Socket.pack_sockaddr_in(80,@host)
-    socket.connect(sockaddr)
-    socket.write("get / http/1.1\r\n\r\n")
-    result = socket.read
-    if result == nil
-      puts "no response from the server"
-      return "unknown OS"
-    end    
-    webServer = result.split("Server: ").last.split("\r\n").first
-
-    if !result.include?("Server:")
-      puts "didnt find Server:"
-      socket.close
+    begin
+      @socket.connect(sockaddr)  
+    rescue Errno::ECONNREFUSED
+      puts "connection Refused"
       return "unknown OS"
     end
+    
+    @socket.write("get / http/1.1\r\n\r\n")
+    begin
+      Timeout::timeout(@timeout) do
+        @result = @socket.read 
+      end
+    rescue Timeout::Error
+      puts "no response from the server"
+      return "unknown OS"
+    end
+      if @result == nil
+        puts "no response from the server"
+        return "unknown OS"      
+      end 
+    webServer = @result.split("Server: ").last.split("\r\n").first
+
+    if !@result.include?("Server:")
+      puts "didnt find Server:"
+      @socket.close
+      return "unknown OS"
+    end
+    @socket.close
     return knownWebServer(webServer.downcase)
   end
 
@@ -45,21 +59,21 @@ class BannerGrabber
   def analyze_ftp
     begin
       Timeout::timeout(@timeout) do
-        socket = TCPSocket.open(@host,21)
-        result = socket.gets    
+        @socket = TCPSocket.open(@host,21)
+        @result = @socket.gets
       end
     rescue Timeout::Error
       return "unknown OS"
     end
-    
-    if result.downcase.include?("ftpd") or result.include?("pure")
-      socket.close
+
+    if @result.downcase.include?("ftpd") or @result.include?("pure")
+      @socket.close
       return "Linux"
-    elsif result.downcase.include?("filezila")
+    elsif @result.downcase.include?("filezila")
       sock.close
       return "windows"
     else
-      socket.close
+      @socket.close
       return "unknown OS"
     end
   end
